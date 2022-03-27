422f3a0dcde1952645fd5222f3ecd3b45673af1f
krzysztofwolny <wolny1337@gmail.com>
2022-03-27 20:52:39 +0200

    Screens draft added

---
+++ screens/ShopsScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const ShopsScreen = () => {
+    return (
+        <View>
+
+        </View>
+    );
+};
+
+export default ShopsScreen
